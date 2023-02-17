6ec477ecefb4fdaef14177c47cdfcb228ea206df
Arsalan <arsalan@gmail.com>
2023-02-17 18:11:08 +0500

    edit work

--- resources/views/Main/usercategories.blade.php
+++ resources/views/Main/usercategories.blade.php
@@ -38,13 +38,13 @@
 
                 <div class="form-group">
                 <label class="weight"  style="margin-right:8px;">Login Date From</label>
-                <input type="Date" name="Date" class="type5">
+                <input type="Date" name="Date" class="type6">
                 
                 <br></div>
 
                 <div class="form-group">
                 <label class="weight"  style="margin-right:30px;">Login Date To</label>
-                <input type="Date" name="Date" class="type5">
+                <input type="Date" name="Date" class="type6">
                 
                 <br></div>
 
