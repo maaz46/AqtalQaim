d55a88018363992ef41cc301caa98ded67838b08
maaz46 <mmushtaq46@gmail.com>
2023-03-04 17:13:18 +0500

    users

--- resources/views/Main/users.blade.php
+++ resources/views/Main/users.blade.php
@@ -1,7 +1,13 @@
 @extends('Main.Layout.layout')
 
+@section('IndividualStyle')
+<style>
+      .page_section:not(:first-child){
+            margin-top:20px;
+      }
+</style>
+@endsection
 @section('MainSection')
-
 <h2 style="color:black;">USERS</h2>
 
 
@@ -27,6 +33,14 @@
                   <div class="row">
 
                         <div class="col-md-6">
+                        <div class="sel">
+                                    <label class="weight" for="user_type" style="margin-right:30px;">Select User Type</label>
+                                    <select id="user_type" name="user_type" required class="Maintype" style="color: white;">
+                                          <option value="" disabled selected>Select A User Type</option>
+                                          <option value="Administrator">Administrator</option>
+                                          <option value="User">User</option>
+                                    </select>
+                              </div>
                               <div class="form">
                                     <label class="weight" style="margin-right:84px;">Full Name</label>
                                     <input type="text" name="full_name" required class="Maintype"><br>
@@ -59,39 +73,19 @@
                                           <input type="checkbox" id="can_change_year" name="can_change_year">
                                     </div>
 
-                                    <div class="sel">
-                                          <label class="weight" for="project_category_id" style="margin-right:30px;">Project Category</label>
-                                          <select id="project_category_id" name="project_category_id" class="Maintype" style="color: white;">
-                                                <option value="" disabled selected>Select A Project Category</option>
-                                                @if(count($project_categories)>0)
-                                                @foreach($project_categories as $key=>$item)
-                                                <option value="{{$item->project_category_id}}">{{$item->project_category}}</option>
-                                                @endforeach
-                                                @endif
-                                          </select>
+                                    
+                                    
+                                    <div class="AssignMoreProjectsDiv">
+                                          
                                     </div>
-
-                                    <div class="sel">
-                                          <label class="weight" for="project_category_id" style="margin-right:30px;">Assign A Project</label>
-                                          <select id="project_id" name="project_id" class="Maintype" style="color: white;">
-                                                <option disabled selected value="">Select A Project Category First</option>
-                                          </select>
+                                    <div class="text-right">
+                                          <button class="btn btn-info btn-sm" type="button" id="BtnAssignMoreProjects">Assign More Projects</button>
                                     </div>
-
-
                               </div>
 
                         </div>
 
                         <div class="col-md-6" style="padding-left: 65px; border-left:2px solid;" id="col1">
-                              <div class="sel">
-                                    <label class="weight" for="user_type" style="margin-right:30px;">Select User Type</label>
-                                    <select id="user_type" name="user_type" required class="Maintype" style="color: white;">
-                                          <option value="" disabled selected>Select A User Type</option>
-                                          <option value="Administrator">Administrator</option>
-                                          <option value="User">User</option>
-                                    </select>
-                              </div>
                               <div class="form-check d-none" id="pages_section">
 
                                     @php
@@ -125,7 +119,8 @@
       </div>
 </form>
 
-<table id="RoleTable" class="table table-responsive-sm">
+<div class="table-responsive mt-3">
+<table id="RoleTable" class="table">
       <thead class="thead-dark">
             <th>Name</th>
             <th>User Name</th>
@@ -157,12 +152,19 @@
             @endif
       </tbody>
 </table>
+</div>
 
 @endsection
 @section('IndividualScript')
 <script>
       $(function() {
+            AssignProjects();
             $('#RoleTable').DataTable();
+
+            $('#BtnAssignMoreProjects').on('click', function(){
+                  AssignProjects();
+            });;
+
             $('#user_type').on('change', function() {
                   if ($(this).val() == "Administrator") {
                         $('#pages_section').addClass('d-none');
@@ -172,40 +174,40 @@
                   }
             });
 
-            $('#user_type').trigger('change');
+            $('#user_type').val('').trigger('change');
       })
 
-      $('#project_category_id, #project_id').val('').prop('selected', true);
-      $('#project_category_id').on('change', function() {
-            var ProjectCategoryID = $(this).val();
-            $('#project_id').css({
-                  'opacity': '0.2',
-                  'pointer-events': 'none'
-            });
-            $.ajax({
-                  url: '/GetProjectsByProjectCategoryID/' + ProjectCategoryID,
-                  type: 'GET',
-                  async: false,
-                  success: function(e) {
-                        $('#project_id').empty();
-                        if (e.length > 0) {
-                              $('#project_id').append('<option disabled selected>Select A Project</option>')
-                              $.each(e, function(i, option) {
+      // $('#project_category_id, #project_id').val('').prop('selected', true);
+      // $(document.body).on('change','#project_category_id', function() {
+      //       var ProjectCategoryID = $(this).val();
+      //       $('#project_id').css({
+      //             'opacity': '0.2',
+      //             'pointer-events': 'none'
+      //       });
+      //       $.ajax({
+      //             url: '/GetProjectsByProjectCategoryID/' + ProjectCategoryID,
+      //             type: 'GET',
+      //             async: false,
+      //             success: function(e) {
+      //                   $('#project_id').empty();
+      //                   if (e.length > 0) {
+      //                         $('#project_id').append('<option disabled selected>Select A Project</option>')
+      //                         $.each(e, function(i, option) {
 
-                                    $('#project_id').append('<option value="' + option.project_id + '"">' +
-                                          option.project_name + '</option>')
-                              });
-                        } else {
-                              $('#project_id').append('<option disabled selected>No Projects Were Found</option >');
-                        }
-                        $('#project_id').css({
-                              'opacity': '1',
-                              'pointer-events': 'all'
-                        });
-                  }
+      //                               $('#project_id').append('<option value="' + option.project_id + '"">' +
+      //                                     option.project_name + '</option>')
+      //                         });
+      //                   } else {
+      //                         $('#project_id').append('<option disabled selected>No Projects Were Found</option >');
+      //                   }
+      //                   $('#project_id').css({
+      //                         'opacity': '1',
+      //                         'pointer-events': 'all'
+      //                   });
+      //             }
 
-            })
-      });
+      //       })
+      // });
 
       $('#userform').on('submit', function(e) {
             var GoodToGo = true;
@@ -229,6 +231,60 @@
             } else {
                   $('#SelectRole_' + PageID).prop('disabled', true);
             }
+      });
+
+      function AssignProjects(){
+            $('.AssignMoreProjectsDiv').append('<div class="ProjectSection page_section"><div class="sel">'
+            +'<label class="weight" for="project_category_id" style="margin-right:30px;">Project Category</label>'
+            +'<select id="" name="project_category_id[]" class="Maintype project_category_id" style="color: white;">'
+            +'<option value="" disabled selected>Select A Project Category</option>'
+            +'@if(count($project_categories)>0)'
+            +'@foreach($project_categories as $key=>$item)'
+            +'<option value="{{$item->project_category_id}}">{{$item->project_category}}</option>'
+            +'@endforeach @endif '
+            +'</select>'
+            +'</div>'
+            
+            +'<div class="sel">'
+            +'<label class="weight" for="project_category_id" style="margin-right:30px;">Assign A Project</label>'
+            +'<select name="project_id[]" class="Maintype project_id" style="color: white;">'
+            +'<option disabled selected value="">Select A Project Category First</option>'
+            +'</select>'
+            +'</div></div>');
+      }
+
+      $(document.body).on('change','.project_category_id',function(){
+            var ProjectSection = $(this).attr('ProjectSection');
+            var ProjectIDSelect = $(this).closest('.ProjectSection').find('.project_id');
+
+                  var ProjectCategoryID = $(this).val();
+            $(ProjectIDSelect).css({
+                  'opacity': '0.2',
+                  'pointer-events': 'none'
+            });
+            $.ajax({
+                  url: '/GetProjectsByProjectCategoryID/' + ProjectCategoryID,
+                  type: 'GET',
+                  async: false,
+                  success: function(e) {
+                        $(ProjectIDSelect).empty();
+                        if (e.length > 0) {
+                              $(ProjectIDSelect).append('<option disabled selected>Select A Project</option>')
+                              $.each(e, function(i, option) {
+
+                                    $(ProjectIDSelect).append('<option value="' + option.project_id + '"">' +
+                                          option.project_name + '</option>')
+                              });
+                        } else {
+                              $(ProjectIDSelect).append('<option disabled selected>No Projects Were Found</option >');
+                        }
+                        $(ProjectIDSelect).css({
+                              'opacity': '1',
+                              'pointer-events': 'all'
+                        });
+                  }
+
+            })
       });
 </script>
 @endsection
